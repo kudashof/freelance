e432178ffb3fd798b94e526eceb9d2f3f21b5eba
AlexKu <kudashof@gmail.com>
2019-12-24 04:11:24 +0300

    Отрабатывает кнопки Фрилансер, Заказчик, Выход. Перебирает элементы формы.

---
+++ script/script.js
@@ -0,0 +1,57 @@
+document.addEventListener('DOMContentLoaded', () => {
+    'use strict';
+/*--------form---------*/
+    const customer = document.getElementById('customer');
+    const freelancer = document.getElementById('freelancer');
+    const blockCustomer = document.getElementById('block-customer');
+    const blockFreelancer = document.getElementById('block-freelancer');
+    const blockChoice = document.getElementById('block-choice');
+    const btnExit = document.getElementById('btn-exit');
+    const formCustomer = document.getElementById('form-customer');
+    const orders = [];
+
+
+    customer.addEventListener('click', () => {
+        blockChoice.style.display = 'none';
+        blockCustomer.style.display = 'block';
+        btnExit.style.display = 'block';
+    });
+
+    freelancer.addEventListener('click', () => {
+        blockChoice.style.display = 'none';
+        blockFreelancer.style.display = 'block';
+        btnExit.style.display = 'block';
+    });
+//кн выход
+    btnExit.addEventListener('click', () => {
+        btnExit.style.display = 'none';
+        blockCustomer.style.display = 'none';
+        blockFreelancer.style.display = 'none';
+        blockChoice.style.display = 'block';
+
+    });
+//перебор эл формы
+    formCustomer.addEventListener('submit', (event) =>{
+        event.preventDefault();
+        debugger;
+        //console.log(formCustomer.elements);
+        const obj = {};
+        for (const elem of formCustomer.elements) {
+            if ((elem.tagName === 'INPUT' && elem.type!== 'radio') ||
+                (elem.type === 'radio' && elem.checked)||
+                elem.tagName === 'TEXTAREA') {
+                obj[elem.name] = elem.value;
+
+                if (elem.type !=='radio') {
+                    elem.value = '';
+                }
+            }
+
+
+        }
+
+        orders.push(obj);
+        console.log(orders);
+
+    })
+});
